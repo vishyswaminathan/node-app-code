999454f357372a3badc19ce3544dab099465ef89
Vishy Swaminathan <vishy.1981@gmail.com>
2025-05-17 18:30:11 -0400

    updating the code

--- node/hello.js
+++ node/hello.js
@@ -1,22 +1,23 @@
 var PORT = 8080;
 var express = require('express');
-
 var helloapp = express();
+
 var msg = "Hello from v2 node app server, ip: ";
 var serverip = "";
+
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
   serverip = add;
   //console.log(msg + serverip + ":" + PORT);
 });
 
+helloapp.get('/', function(req, res) {
+  res.send('Welcome to the Node.js App!');
+});
+
 helloapp.get('/hello', function(req, res) {
-
-//    res.setHeader("Content-Type", "application/json");
-
-    res.send(msg + serverip + ":" + PORT);
-
+  res.send(msg + serverip + ":" + PORT);
 });
 
 helloapp.listen(PORT, function() {
   console.log('Hello app listening on port ' + PORT)
-})
+});
